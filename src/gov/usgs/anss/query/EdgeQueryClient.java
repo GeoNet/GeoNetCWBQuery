2b433a6877ec64e88cd8c4158d2a7e78e740e955
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2010-04-28 03:18:23 +0000

    Added GNU GPL3.0

--- src/gov/usgs/anss/query/EdgeQueryClient.java
+++ src/gov/usgs/anss/query/EdgeQueryClient.java
@@ -1,10 +1,20 @@
 /*
- * EdgeQueryClient.java
+ * Copyright 2006, United States Geological Survey or
+ * third-party contributors as indicated by the @author tags.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Created on February 9, 2006, 1:25 PM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
  */
 package gov.usgs.anss.query;
 
